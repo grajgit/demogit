fc485dd4a2da72c8c69d17e107c3f9a544e96c3a
grajgit <rajeshudt12@gmail.com>
2022-10-14 14:02:55 +0530

    new code

---
+++ src/org/ttttt/ClassInf.java
@@ -0,0 +1,22 @@
+package org.ttttt;
+
+public class ClassInf {
+private void trainer() {
+	// TODO Auto-generated method stub
+System.out.println("shiva");
+}
+private void traininf() {
+	// TODO Auto-generated method stub
+System.out.println("testing");
+}
+private void days() {
+	// TODO Auto-generated method stub
+System.out.println("45");
+}
+public static void main(String[] args) {
+	ClassInf d = new ClassInf();
+	d.traininf();
+	d.days();
+	d.trainer();
+}
+}
